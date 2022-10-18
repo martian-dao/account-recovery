75dce2f09e423ceff4ca4f933e6eed107d8ca847
MartianAbhishek <abhishek@martiandao.xyz>
2022-10-19 02:09:47 +0530

    css change

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import Home from "./pages/Home";
 import Logo from "./icons/logo.png";
 
 function App() {
-  const websiteLink = "https://www.martianwallet.xyz/"
+  const websiteLink = "https://www.martianwallet.xyz/";
 
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full w-full">
-      {
-          <Home />
-      }
-          <a href={websiteLink} target="_blank" style={{ top: "30px", left: "70px" }} className="absolute_div">
-                    <img src={Logo} alt="Martian" />
-                    <p color="white">Martian</p>
-                </a>
+        {<Home />}
+        <a
+          href={websiteLink}
+          target="_blank"
+          style={{ top: "30px", left: "70px" }}
+          className="absolute_div"
+        >
+          <img src={Logo} alt="Martian" />
+          <p
+            size="16"
+            lineheight="24"
+            letterspacing="-0.01em"
+            fontfamily="MigraLight"
+            marginleft="6"
+            fontweight="600"
+          >
+            Martian
+          </p>
+        </a>
       </div>
     </>
   );
--- src/utils/mnemonic.js
+++ src/utils/mnemonic.js
@@ -1,12 +1,7 @@
-import * as bip39 from "@scure/bip39";
-import * as english from "@scure/bip39/wordlists/english";
 import { WalletClient } from "@martiandao/aptos-web3-bip44.js";
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
 export async function importWallet(code) {
-  if (!bip39.validateMnemonic(code, english.wordlist)) {
-    return Promise.reject(new Error("Incorrect mnemonic passed"));
-  }
   const account = WalletClient.getAccountFromMnemonic(code);
   return [account.toPrivateKeyObject()];
 }
