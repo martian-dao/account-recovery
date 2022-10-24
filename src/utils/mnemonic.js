5cb69a7fbabc559612bb5238c9bd65b7ba1af600
MartianAbhishek <abhishek@martiandao.xyz>
2022-10-24 15:58:54 +0530

    added current account support

--- src/utils/mnemonic.js
+++ src/utils/mnemonic.js
@@ -1,4 +1,6 @@
-import { WalletClient } from '@martiandao/aptos-web3-bip44.js'
+import { WalletClient } from '@martiandao/aptos-web3-bip44-recovery.js'
+import { WalletClient as WalletClientCurrent } from '@martiandao/aptos-web3-bip44.js'
+
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
 
@@ -7,10 +9,13 @@ const DERIVATION_PATH_2 = "m/44'/637'/I'/0/J"
 const DERIVATION_PATH_3 = "m/44'/637'/I'/0'/J'"
 
 const walletClient = new WalletClient('https://rpc.mainnet.aptos.fernlabs.xyz/v1', 'dummy')
+const walletClientCurrent = new WalletClientCurrent('https://rpc.mainnet.aptos.fernlabs.xyz/v1', 'dummy')
 
 export async function importWallet(code) {
   const account1 = await walletClient.importWallet(code, DERIVATION_PATH_1);
   const account2 = await walletClient.importWallet(code, DERIVATION_PATH_2, );
   const account3 = await walletClient.importWallet(code, DERIVATION_PATH_3, );
-  return [...account1.accounts, ...account2.accounts, ...account3.accounts,];
+  const account5 = await walletClientCurrent.importWallet(code);
+
+  return [...account1.accounts, ...account2.accounts, ...account3.accounts, ...account5.accounts];
 }
